f58dada2dcc6184d9efd58a6f936dfb196e6f437
dennischow1 <dennis.ckchow@gmail.com>
2018-10-18 21:20:45 +1100

    eval

--- evaluation_single_stream.py
+++ evaluation_single_stream.py
@@ -5,18 +5,12 @@ Created on Thu Oct 18 19:31:28 2018
 @author: DeSI-Anjin, Dennis Chow
 """
 
-from sklearn.linear_model import BayesianRidge, LinearRegression
-from sklearn import linear_model
+#from sklearn.linear_model import BayesianRidge, LinearRegression
+#from sklearn import linear_model
 import numpy as np
 from statsmodels.tsa.ar_model import AR
 from random import random
-# contrived dataset
-data = [x + random() for x in range(1, 100)]
-# fit model
-model = AR(data)
-model_fit = model.fit()
-# make prediction
-yhat = model_fit.predict(len(data), len(data))
+from matplotlib import pyplot as plt
 
 data_path = 'evaluation_stream/'
 
@@ -27,20 +21,36 @@ with open(file_list_path) as f:
         file_list.append(line.replace('\n',''))
 
 predict_period = 14
-reg = linear_model.BayesianRidge()
+#reg = linear_model.BayesianRidge()
 for s in file_list:
     s_data = np.loadtxt(data_path+s,delimiter=',')
     s_y = s_data[:,1]
     s_y_size = s_y.shape[0]
+    model = AR(s_y)
+    model_fit = model.fit()
+    yhat = model_fit.predict(len(s_y), len(s_y))
+    plt.plot(s_y)
+    plt.plot(yhat)
     
-    for slide in range(100, s_y_size - predict_period):
-        print (slide)
-        X = s_y[:slide]
-        Y = s_y[slide:slide+predict_period]
-        reg.fit(X, Y)
-    print(s_y_size)
-    
-    
+plt.show()
+# =============================================================================
+#     for slide in range(100, s_y_size - predict_period):
+#         print (slide)
+#         X = s_y[:slide]
+#         Y = s_y[slide:slide+predict_period]
+#         model = AR(X)
+#         model_fit = model.fit()
+#         yhat = model_fit.predict(len(data), len(data))
+#     print(s_y_size)
+#     
+#     # contrived dataset
+#     data = [x + random() for x in range(1, 100)]
+#     # fit model
+#     model = AR(data)
+#     model_fit = model.fit()
+#     # make prediction
+#     yhat = model_fit.predict(len(data), len(data))
+# =============================================================================
     
     
     
