d92787360c7a65ecc7fe0a6e466f2137125b8d5e
dennischow1 <dennis.ckchow@gmail.com>
2018-10-23 19:26:01 +1100

    update

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,6 +8,7 @@ Created on Mon Oct 22 19:30:55 2018
 import numpy as np
 from scipy import stats
 from sklearn.linear_model import BayesianRidge, LinearRegression
+import matplotlib.pyplot as plt
 
 def data_preprocessing(file_list):
 
@@ -40,8 +41,8 @@ if __name__ =='__main__':
             
     s_y = data_preprocessing(file_list)
     
-    X = np.ndarray(shape=(59,11))
-    X_temp = []
+#    X = np.ndarray(shape=(59,11))
+#    X_temp = []
     
     '''
     for i in range(0,59):
@@ -56,31 +57,36 @@ if __name__ =='__main__':
     for key, v in s_y.items():
         train_data = np.vstack([train_data, np.array(v)])
         
-    leg = 2
+    lag = 2
     
-    sample_x = np.transpose(train_data[:,:-leg])
+    sample_x = np.transpose(train_data[:,:-lag])
     
-    for i in range(1, leg):
-        sample_x = np.hstack([sample_x, np.transpose(train_data[:,i:-(leg-i)])])
+    for i in range(1, lag):
+        sample_x = np.hstack([sample_x, np.transpose(train_data[:,i:-(lag-i)])])
     
     
     # use for loop to extract sample_y_si and create train_si
-    sample_y_s0 = np.transpose(train_data[0,:-leg][np.newaxis])
+    sample_y_s0 = np.transpose(train_data[0,:-lag][np.newaxis])
     
     train_s0 = np.hstack([sample_x, sample_y_s0])
     
     
     
-    sample_x_s1 = train_data[:,0]
-    sample_y_s1 = train_data[0,1]
+#    sample_x_s0 = train_data[:,0]
+#    sample_y_s0 = train_data[0,1]
+#    
+#    sample_s1 = np.hstack([sample_x_s1, sample_y_s1])
     
-    sample_s1 = np.hstack([sample_x_s1, sample_y_s1])
+    
+    
     
     #train_data = np.transpose(train_data)
-    print(sample_s1)
     
-#    reg = linear_model.BayesianRidge
-#    reg.fit(X[:,:-1],X[:,-1])
+    
+    reg_s0 = BayesianRidge()
+    reg_s0.fit(train_s0[:60,:-1],train_s0[1:61,-1])
+    yhat = reg_s0.predict(train_s0[61,:-1])
+    plt.plot(reg_s0.coef_)
     
     
         
